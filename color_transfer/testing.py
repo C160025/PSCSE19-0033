98924fbea99683e95a86bced363bbd72c4d2d247
C160025 <54878726+C160025@users.noreply.github.com>
2020-05-10 22:45:57 +0800

    update on 10/5/2020

--- color_transfer/testing.py
+++ color_transfer/testing.py
@@ -61,9 +61,9 @@ image_skimage_rgb2 = color.lab2rgb(image_skimage_lab.astype("uint8"))
 image_opencv_bgr = cv2.imread(source, cv2.IMREAD_COLOR)
 opencv_bgr = image_opencv_bgr
 
-print(image_opencv_bgr[:, :, 0])
-print(image_opencv_bgr[:, :, 1])
-print(image_opencv_bgr[:, :, 2])
+# print(image_opencv_bgr[:, :, 0])
+# print(image_opencv_bgr[:, :, 1])
+# print(image_opencv_bgr[:, :, 2])
 image_opencv_xyz = cv2.cvtColor(image_opencv_bgr, cv2.COLOR_RGB2XYZ)
 # print(f'{image_opencv_xyz.min()}, {image_opencv_xyz.max()}')
 image_opencv_lab = cv2.cvtColor(image_opencv_bgr, cv2.COLOR_BGR2Lab)
@@ -113,7 +113,65 @@ array_image_pillow = np.array(image_pillow)[:1]
 
 
 
+# Matrix
+def ct_rgb2lab(image_rgb, log10):
+    height = image_rgb.shape[0]
+    width = image_rgb.shape[1]
+    img_lab = np.zeros_like(image_rgb, dtype=np.float32)
+    rgb2xyz_eq = [[0.5141, 0.3239, 0.1604],
+                  [0.2651, 0.6702, 0.0641],
+                  [0.0241, 0.1228, 0.8444]]
+    xyz2lms_eq = [[0.3897, 0.6890, -0.0787],
+                  [-0.2298, 1.1834, 0.0464],
+                  [0.0000, 0.0000, 1.0000]]
+    rgb2lms_eq1 = np.matmul(xyz2lms_eq, rgb2xyz_eq)
+    rgb2lms_eq2 = [[0.3811, 0.5783, 0.0402],
+                   [0.1967, 0.7244, 0.0782],
+                   [0.0241, 0.1288, 0.8444]]
+    lms2lab_eq1 = [[1/math.sqrt(3), 0.0000, 0.0000],
+                   [0.0000, 1/math.sqrt(6), 0.0000],
+                   [0.0000, 0.0000, 1/math.sqrt(2)]]
+    lms2lab_eq2 = [[1.0000, 1.0000, 1.0000],
+                   [1.0000, 1.0000, -2.0000],
+                   [1.0000, -1.0000, 0.0000]]
+    lms2lab_eq = np.matmul(lms2lab_eq1, lms2lab_eq2)
+    for x in range(0, width):
+        for y in range(0, height):
+            img_lab[y, x] = np.matmul(rgb2lms_eq1, image_rgb[y, x])
+            if (log10 == 1):
+                img_lab[y, x] = np.log10(img_lab[y, x])
+            img_lab[y, x] = np.matmul(lms2lab_eq, img_lab[y, x])
+    return img_lab
 
+def ct_lab2rgb(image_lab, power10):
+    height = image_lab.shape[0]
+    width = image_lab.shape[1]
+    img_rgb = np.zeros_like(image_lab, dtype=np.float32)
+    lab2lms_eq1 = [[1.0000, 1.0000, 1.0000],
+                   [1.0000, 1.0000, -1.0000],
+                   [1.0000, -2.0000, 0.0000]]
+    lab2lms_eq2 = [[math.sqrt(3)/3, 0.0000, 0.0000],
+                   [0.0000, math.sqrt(6)/6, 0.0000],
+                   [0.0000, 0.0000, math.sqrt(2)/2]]
+    lab2lms_eq = np.matmul(lab2lms_eq1, lab2lms_eq2)
+    lms2rgb_eq = [[4.4679, -3.5876, 0.1193],
+                  [-1.2186, 2.3809, -0.1624],
+                  [0.0497, -0.2439, 1.2045]]
+    for x in range(0, width):
+        for y in range(0, height):
+            img_rgb[y, x] = np.matmul(lab2lms_eq, image_lab[y, x])
+            #if (power10 == 1):
+            img_rgb[y, x] = np.power(10, img_rgb[y, x]) # something wrong here !!!!
+            img_rgb[y, x] = np.matmul(lms2rgb_eq, img_rgb[y, x])
+    return img_rgb
+
+
+image_opencv_bgr = cv2.imread(source, cv2.IMREAD_COLOR)
+image_opencv_rgb =  cv2.cvtColor(image_opencv_bgr, cv2.COLOR_BGR2RGB)
+test13 = ct_rgb2lab(image_opencv_rgb, 1)
+# print(test13[:1])
+test14 = ct_lab2rgb(test13, 1)
+print(test14[:1])
 
 
 # print(image.size)
